830c82c67ac72b71b34878280d46a879fd6a7eab
Navinda Chandrasiri <adilanga@gmail.com>
2022-11-02 11:27:23 +0530

    Auth0 Impl

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Home from "./pages/Home";
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <h1>Home</h1>
+                <Home/>
             </header>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+// import ReactDOM from "react-dom";
 import App from './App';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-reportWebVitals();
+root.render(
+    <React.StrictMode>
+        <Auth0Provider
+            domain="paradocx.us.auth0.com"
+            clientId="DCbhi0zAeI59ihuuQVc8UdDbrRX3NCDR"
+            redirectUri={window.location.origin}
+        >
+            <App/>
+        </Auth0Provider>
+    </React.StrictMode>
+);
